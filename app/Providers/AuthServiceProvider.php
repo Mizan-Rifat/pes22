9bd0773b09e2eb6cd21f4deb88be578ea1a09fe4
Mizan-Rifat <mizanrifat001@gmail.com>
2022-02-20 23:52:08 +0600

    super admin gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,6 +21,8 @@ class AuthServiceProvider extends ServiceProvider {
 	public function boot() {
 		$this->registerPolicies();
 
-		//
+		Gate::before(function ($user, $ability) {
+			return $user->hasRole('super_admin') ? true : null;
+		});
 	}
 }
