801b14b131f73c190c7566cbc961b91808235d3a
Mizan-Rifat <mizanrifat001@gmail.com>
2022-02-20 23:28:49 +0600

    role permission controller

--- app/Http/Controllers/RolePermissionController.php
+++ app/Http/Controllers/RolePermissionController.php
@@ -7,60 +7,67 @@ use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
-class RolePermission extends Controller {
+class RolePermissionController extends Controller {
 
-	public function givePermissionToUser(Request $request) {
-		$validatedData = $request->validate([
-			'user' => ['required', 'numeric', 'exists:users,id'],
-			'permission' => ['required', 'array'],
-			'permission.*' => ['numeric', 'exists:permissions,id'],
-		]);
+	// public function givePermissionToUser(Request $request) {
+	// 	$validatedData = $request->validate([
+	// 		'user' => ['required', 'numeric', 'exists:users,id'],
+	// 		'permission' => ['required', 'array'],
+	// 		'permission.*' => ['numeric', 'exists:permissions,id'],
+	// 	]);
 
-		$user = User::find($validatedData['user']);
-		return $user->givePermissionTo($validatedData['permission']);
-	}
+	// 	$user = User::find($validatedData['user']);
+	// 	return $user->givePermissionTo($validatedData['permission']);
+	// }
 
-	public function revokePermissionFromUser(User $user, Permission $permission) {
-		return $user->revokePermissionTo($permission);
-	}
+	// public function revokePermissionFromUser(User $user, Permission $permission) {
+	// 	return $user->revokePermissionTo($permission);
+	// }
 
 	public function givePermissionToRole(Request $request) {
+		$this->authorize('assignRolePermission', Role::class);
 		$validatedData = $request->validate([
 			'role' => ['required', 'numeric', 'exists:roles,id'],
-			'permission' => ['required', 'array'],
+			'permission' => ['array'],
 			'permission.*' => ['numeric', 'exists:permissions,id'],
 		]);
 		$role = Role::find($validatedData['role']);
-		return $role->givePermissionTo($validatedData['permission']);
+		return $role->syncPermissions($validatedData['permission']);
 	}
 
 	public function revokePermissionFromRole(Role $role, Permission $permission) {
+		$this->authorize('revokeRolePermission', Role::class);
 		return $role->revokePermissionTo($permission);
 	}
 
 	public function assignRoleToUser(Request $request) {
+		$this->authorize('assignRolePermission', Role::class);
 		$validatedData = $request->validate([
 			'user' => ['required', 'numeric', 'exists:users,id'],
-			'role' => ['required', 'array'],
+			'role' => ['array'],
 			'role.*' => ['numeric', 'exists:roles,id'],
 		]);
 		$user = User::find($validatedData['user']);
-		return $user->assignRole($validatedData['role']);
+		return $user->syncRoles($validatedData['role']);
 	}
 
 	public function removeRoleFromUser(User $user, Role $role) {
+		$this->authorize('revokeRolePermission', Role::class);
 		return $user->removeRole($role);
 	}
 
 	public function getAllPermissionsOfUser(User $user) {
+		$this->authorize('viewRolePermission', Role::class);
 		return $user->getAllPermissions();
 	}
 
 	public function getAllRolesOfUser(User $user) {
+		$this->authorize('viewRolePermission', Role::class);
 		return $user->getRoleNames();
 	}
 
 	public function getAllPermissionsOfRole(Role $role) {
+		$this->authorize('viewRolePermission', Role::class);
 		return $role->getAllPermissions();
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
-use App\Http\Controllers\RolePermission;
+use App\Http\Controllers\RolePermissionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,14 +27,14 @@ Route::resource('roles', RoleController::class, ['except' => ['create', 'edit']]
 Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
 
 
-Route::get('user/{user}/permission', [RolePermission::class, 'getAllPermissionsOfUser']);
-Route::post('user/permission', [RolePermission::class, 'givePermissionToUser']);
-Route::delete('user/{user}/permission/{permission}', [RolePermission::class, 'revokePermissionFromUser']);
+Route::get('user/{user}/permission', [RolePermissionController::class, 'getAllPermissionsOfUser']);
+Route::post('user/permission', [RolePermissionController::class, 'givePermissionToUser']);
+Route::delete('user/{user}/permission/{permission}', [RolePermissionController::class, 'revokePermissionFromUser']);
 
-Route::get('user/{user}/role', [RolePermission::class, 'getAllRolesOfUser']);
-Route::post('user/role', [RolePermission::class, 'assignRoleToUser']);
-Route::delete('user/{user}/role/{role}', [RolePermission::class, 'removeRoleFromUser']);
+Route::get('user/{user}/role', [RolePermissionController::class, 'getAllRolesOfUser']);
+Route::post('user/role', [RolePermissionController::class, 'assignRoleToUser']);
+Route::delete('user/{user}/role/{role}', [RolePermissionController::class, 'removeRoleFromUser']);
 
-Route::get('role/{role}/permission', [RolePermission::class, 'getAllPermissionsOfRole']);
-Route::post('role/permission', [RolePermission::class, 'givePermissionToRole']);
-Route::delete('role/{role}/permission/{permission}', [RolePermission::class, 'revokePermissionFromRole']);
+Route::get('role/{role}/permission', [RolePermissionController::class, 'getAllPermissionsOfRole']);
+Route::post('role/permission', [RolePermissionController::class, 'givePermissionToRole']);
+Route::delete('role/{role}/permission/{permission}', [RolePermissionController::class, 'revokePermissionFromRole']);
