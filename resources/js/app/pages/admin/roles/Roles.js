b32c4a9118988b113d8227a6092a506da5b0e1d0
Mizan-Rifat <mizanrifat001@gmail.com>
2022-02-26 22:20:55 +0600

    mui table

--- resources/js/app/pages/admin/roles/Roles.js
+++ resources/js/app/pages/admin/roles/Roles.js
@@ -1,7 +1,17 @@
+import { Box, Paper } from '@mui/material';
+import PaperHeader from 'components/paper/PaperHeader';
 import React from 'react';
+import TableExample from './TableExample';
 
 const Roles = () => {
-  return <div>Roles</div>;
+  return (
+    <Paper variant="layout" sx={{ width: '100%' }}>
+      <PaperHeader title="Roles" subTitle="Choose user roles" color="primary" />
+      <Box sx={{ px: 2 }}>
+        <TableExample />
+      </Box>
+    </Paper>
+  );
 };
 
 export default Roles;
